f58a21343d587e9df5c7874e0e607438cd17a937
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-01-18 19:13:31 +0600

    Update Header and add menu with links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
+import { Link } from "react-router-dom";
+
 const Header = () => (
-    <div className="header">
-        <h1>This is the Header</h1>
-    </div>
+  <div className="header">
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+      <div class="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <Link class="nav-link active" to="/">
+              Posts
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/photos">
+              Photos
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/albums">
+              Albums
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/gallery">
+              Gallery
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/users">
+              Users
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 );
 
 export default Header;
---
+++ src/components/Photos.js
@@ -0,0 +1,5 @@
+const Photos = () => (
+    <h1>This is Photos page</h1>
+);
+
+export default Photos;
