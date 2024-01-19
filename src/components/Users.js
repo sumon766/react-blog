7c2b1be802e35e9ecb0bdca2ac4a10d06960c893
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-01-20 01:16:54 +0600

    Fetched and printed users successfully from the API

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,37 @@
-const Users = () => (
-    <div className="users">
-        <h1>This is the Users Page</h1>
-    </div>
-);
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from '../redux/actions/userSlice';
+
+const Users = () => {
+    const dispatch = useDispatch();
+    const users = useSelector(state => state.user.users);
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    return (
+        <div className="users">
+            <div className="page-title">
+                <h1>List of Users</h1>
+                {users && users.length > 0 ? users.map(user => (
+                    <div className="user" key={user.id}>
+                        Name: {user.name}<br/>
+                        Username: {user.username}<br/>
+                        Email: {user.email}<br/>
+                        Address: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode} <br/>
+                        Geo Location: {user.address.geo.lat}, {user.address.geo.lng}<br/>
+                        Phone: {user.phone}<br/>
+                        Website: {user.website}<br/>
+                        {user.company.name}<br/>
+                        <br/>
+                    </div>
+                )) : (
+                    <h2>Loading user data...</h2>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default Users;
