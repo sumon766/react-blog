ea1098fb502883473ed92fe2e4f9c44c554b76e5
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-01-22 14:11:05 +0600

    PhotoSlice Created

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,5 +1,12 @@
-const Photos = () => (
-  <h1>This is Photos page</h1>
-);
+import { useEffect } from "react";
+import { UseSelector, useDispatch } from "react-redux";
+import { fetchPhotos } from "../redux/actions/photoSlice";
+
+const Photos = () => {
+
+  return (
+    <h1>This is Photos page</h1>
+  );
+}
 
 export default Photos;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './actions/userSlice';
 import postReducer from './actions/postSlice';
+import photoReducer from './actions/photoSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     post: postReducer,
+    photo: photoReducer
   },
 });
 
