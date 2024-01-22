297e746899caeecf859a779c9a72e5d729342158
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-01-22 13:30:22 +0600

    Posts fetched from the API and displayed on post-page

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -11,9 +11,25 @@ const Posts = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>This is Posts Page.</h1>
-    </div>
+    <div className="post-area">
+      <div className="page-title">
+        <h1>List of Posts</h1>
+      </div>
+      <div className="posts">
+        {posts && posts.length > 0 ? posts.map((post) => (
+          <div className="post">
+            {post.id} <br/>
+            {post.userId}<br/>
+            {post.title}<br/>
+            {post.body}
+          </div>
+        )) : (
+          <div className="loading">
+            <h3>Loading posts</h3>
+          </div>
+        )}
+      </div>
+    </div>  
   );
 }
 
