eb975e8c2f04603eb55e141a48f352181b71fbdd
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-01-26 17:45:30 +0600

    Update header to add comments page link

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,6 +30,11 @@ const Header = () => (
               Users
             </Link>
           </li>
+          <li className="nav-item">
+            <Link class="nav-link" to="/comments">
+              Comments
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
